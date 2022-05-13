e28201d06dd2657f64e5ff5ff0f8badc90e0890f
Facundo Bravo <facundobravot@gmail.com>
2022-05-13 19:59:56 -0300

    0.0.3

--- Assets/Scripts/Dollarize.cs
+++ Assets/Scripts/Dollarize.cs
@@ -1,20 +1,31 @@
+//En este proyecto se utilizan billetes argentinos como marcadores de RA. La imagen desplegada es la de un dolar. 
+//Este Script se utiliza para convertir el valor del billete visualizado y reproducirlo sobre la imagen del dolar.
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Dollarize : MonoBehaviour
 {
     public float dollar = 117.25f;
     public float pesos;    
     public float dollarized;
+
+    public TextMeshPro[] textos;
+
     void Start()
     {
-        Dollarization(pesos);
+        textos = new TextMeshPro[gameObject.transform.childCount];
+        textos = gameObject.GetComponentsInChildren<TextMeshPro>();
+
+        
     }
     
     void Update()
     {
-        
+        Dollarization(pesos);
     }
 
     public void Dollarization(float _pesos)
@@ -22,5 +33,10 @@ public class Dollarize : MonoBehaviour
         dollarized = _pesos / dollar;
         dollarized = Mathf.Round(dollarized * 100.0f) * 0.01f;
         print(dollarized);
+
+        for ( int i = 0 ; i < textos.Length ; i ++ )
+        {
+            textos[i].text = dollarized.ToString();
+        }
     }
 }
